016d0202ceb89663892294ca2fe68ccd306aec2a
Maxpro-dk <toyigbemaximed@gmail.com>
2025-07-01 00:45:03 +0100

    feat: initialisation du  projet

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -20,18 +20,7 @@
       class="flex items-center mt-2 grow sm:mt-0 {{ (Request::is('rtl') ? '' : 'sm:mr-6') }}md:mr-0 lg:flex lg:basis-auto">
 
       <div class="flex items-center md:ml-auto md:pr-4">
-        <!-- pro btn  -->
-        <a class="inline-block w-full px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all ease-in border-0 rounded-lg select-none shadow-soft-md bg-150 bg-x-25 leading-pro text-size-xs bg-gradient-fuchsia hover:shadow-soft-2xl hover:scale-102"
-          href="https://www.creative-tim.com/product/soft-ui-dashboard-tall" target="_blank">Free download</a>
-        <div class="ml-2 relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease-soft">
-          <span
-            class="text-size-sm ease-soft leading-5.6 absolute z-50 {{ (Request::is('rtl') ? '-mr-px border-l-0 rounded-tl-none rounded-bl-none' : '-ml-px border-r-0 rounded-tr-none rounded-br-none') }} flex h-full items-center whitespace-nowrap rounded-lg border border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
-            <i class="fas fa-search"></i>
-          </span>
-          <input type="text"
-            class="{{ (Request::is('rtl') ? 'pr-8.75 -mr-px pl-0' : 'pl-8.75 -ml-px pr-3') }} text-size-sm focus:shadow-soft-primary-outline ease-soft w-1/100 leading-5.6 relative block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 text-gray-700 transition-all placeholder:text-gray-500 focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
-            placeholder="Type here..." />
-        </div>
+       
       </div>
       <ul
         class="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full {{ (Request::is('rtl') ? 'pr-10 ml-0 mr-auto' : '') }}">
--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -16,15 +16,14 @@
         <img src="../assets/img/logo-ct.png"
           class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
         <span
-          class="{{ (Request::is('rtl') ? 'mr-1' : 'ml-1') }} font-semibold transition-all duration-200 ease-nav-brand">Soft
-          UI Dashboard</span>
+          class="{{ (Request::is('rtl') ? 'mr-1' : 'ml-1') }} font-semibold transition-all duration-200 ease-nav-brand">GESTION D'ECOLE</span>
       </a>
     </div>
 
     <hr
       class="h-px mt-0 bg-transparent {{ (Request::is('virtual-reality') ? 'bg-gradient-horizontal-dark' : 'via-black/40 bg-gradient-to-r from-transparent to-transparent') }} " />
 
-    <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
+    <div class="items-center block w-auto max-h-screen overflow-auto  grow basis-full">
       <ul class="flex flex-col pl-0 mb-0">
         <li class="mt-0.5 w-full">
           <a class="py-2.7 text-size-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors
@@ -53,14 +52,14 @@
               </svg>
             </div>
             <span
-              class="{{ (Request::is('rtl') ? 'mr-1' : 'ml-1') }} duration-300 opacity-100 pointer-events-none ease-soft">Dashboard</span>
+              class="{{ (Request::is('rtl') ? 'mr-1' : 'ml-1') }} duration-300 opacity-100 pointer-events-none ease-soft">Tableau De Board</span>
           </a>
         </li>
 
         <li class="w-full mt-4">
           <h6
             class="{{ (Request::is('rtl') ? 'pr-6 mr-2' : 'pl-6 ml-2') }} font-bold leading-tight uppercase text-size-xs opacity-60">
-            Laravel examples</h6>
+            PARAMETTRE</h6>
         </li>
 
         <li class="mt-0.5 w-full">
@@ -123,7 +122,7 @@
         <li class="w-full mt-4">
           <h6
             class="{{ (Request::is('rtl') ? 'pr-6 mr-2' : 'pl-6 ml-2') }} font-bold leading-tight uppercase text-size-xs opacity-60">
-            Page examples</h6>
+            SYSTEME DE GESTION</h6>
         </li>
 
 
@@ -265,7 +264,7 @@
           </a>
         </li>
 
-        <li class="w-full mt-4">
+        {{-- <li class="w-full mt-4">
           <h6
             class="{{ (Request::is('rtl') ? 'pr-6 mr-2' : 'pl-6 ml-2') }} font-bold leading-tight uppercase text-size-xs opacity-60">
             Account pages</h6>
@@ -370,7 +369,7 @@
               class="{{ (Request::is('rtl') ? 'mr-1' : 'ml-1') }} duration-300 opacity-100 pointer-events-none ease-soft">Sign
               Up</span>
           </a>
-        </li>
+        </li> --}}
       </ul>
     </div>
 
@@ -380,31 +379,10 @@
         class="hidden after:bg-gradient-dark-gray after:bg-gradient-cyan after:bg-gradient-orange after:bg-gradient-lime after:bg-gradient-red after:bg-gradient-slate">
       </p>
 
-      <div
-        class="after:opacity-65 after:bg-gradient-slate relative flex min-w-0 flex-col items-center break-words rounded-2xl border-0 border-solid border-blue-900 bg-white bg-clip-border shadow-none after:absolute after:top-0 after:bottom-0 after:left-0 after:z-10 after:block after:h-full after:w-full after:rounded-2xl after:content-['']"
-        sidenav-card>
-        <div class="mb-7.5 absolute h-full w-full rounded-2xl bg-cover bg-center"
-          style="background-image: url('../assets/img/curved-images/white-curved.jpeg')"></div>
-        <div class="relative z-20 flex-auto w-full p-4 text-left text-white">
-          <div
-            class="flex items-center justify-center w-8 h-8 mb-4 text-center bg-white bg-center rounded-lg icon shadow-soft-2xl">
-            <i class="top-0 z-10 text-transparent ni ni-diamond text-size-lg bg-gradient-slate bg-clip-text opacity-80"
-              sidenav-card-icon></i>
-          </div>
-          <div class="transition-all duration-200 ease-nav-brand">
-            <h6 class="mb-0 text-white {{ (Request::is('rtl') ? 'text-right' : '') }}">Need help?</h6>
-            <p
-              class="mt-0 mb-4 font-semibold leading-tight text-size-xs {{ (Request::is('rtl') ? 'text-right' : '') }}">
-              Please check our docs</p>
-            <a href="https://www.creative-tim.com/learning-lab/tailwind/html/quick-start/soft-ui-dashboard/"
-              target="_blank"
-              class="inline-block w-full px-8 py-2 mb-0 font-bold text-center text-black uppercase transition-all ease-in bg-white border-0 border-white rounded-lg shadow-soft-md bg-150 leading-pro text-size-xs hover:shadow-soft-2xl hover:scale-102">Documentation</a>
-          </div>
-        </div>
-      </div>
+     
       <!-- pro btn  -->
       <a class="inline-block w-full px-6 py-3 my-4 font-bold text-center text-white uppercase align-middle transition-all ease-in border-0 rounded-lg select-none shadow-soft-md bg-150 bg-x-25 leading-pro text-size-xs bg-gradient-fuchsia hover:shadow-soft-2xl hover:scale-102"
-        href="https://www.creative-tim.com/product/soft-ui-dashboard-pro-tall" target="_blank">Upgrade to pro</a>
+        href="https://www.creative-tim.com/product/soft-ui-dashboard-pro-tall" target="_blank">Deconnexion</a>
     </div>
   </aside>
 
